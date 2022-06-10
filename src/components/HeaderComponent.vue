4befc78ac82e1b22a64acc8cbbf8abaa5659a1c7
sheepisnotgoat <abhijitsing.961@gmail.com>
2022-06-10 11:06:09 +0530

    main header created

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="main-header-container">
+    <div class="logo-container">
+      <img class="logo" src="/ikea.svg" alt="ikea_logo" />
+    </div>
+
+    <div class="user-action-container">
+      <div class="search-container">
+        <input
+          class="search-box"
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search..."
+          v-model="lv_search"
+        />
+        <button class="search-button">
+          <img
+            class="search-icon"
+            src="/search-button.svg"
+            alt="search!"
+            @click="onDivClick"
+          />
+        </button>
+      </div>
+    </div>
+
+    <div class="user-stat-container">
+      <button class="user-button">
+        <img src="/user-icon.png" alt="User" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      lv_search: "",
+    };
+  },
+  methods: {
+    onDivClick() {
+      alert(this.lv_search);
+    },
+  },
+};
+</script>
+
+<style>
+.logo {
+  width: 100px;
+  height: 30px;
+}
+.logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.main-header-container {
+  display: flex;
+  justify-content: space-between;
+}
+.search-box {
+  padding: 10px;
+  width: 90%;
+  height: 25px;
+  margin-right: 1%;
+  background: transparent;
+  border: 1px solid black;
+  border-radius: 25px 25px 25px 25px;
+}
+.search-box:focus {
+  outline: none;
+}
+.user-action-container {
+  width: 35%;
+  display: flex;
+}
+.user-button {
+  background: transparent;
+  border: none;
+}
+.user-stat-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.search-container {
+  display: flex;
+  width: 100%;
+}
+.search-button {
+  width: 45px;
+  height: 45px;
+  background: transparent;
+  border: none;
+}
+.search-icon {
+  width: 25px;
+  height: 25px;
+}
+</style>
---
+++ src/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "/src/components/HeaderComponent.vue";
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style></style>
